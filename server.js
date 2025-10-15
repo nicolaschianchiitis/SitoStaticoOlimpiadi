44804f7a3a75cca6fe34988e142046548f1b9f99
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2025-10-16 00:07:04 +0200

    Iniziato server.js e sistemato nome immagine logo olimpiadi

---
+++ server.js
@@ -0,0 +1,57 @@
+// Moduli utilizzati
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+// Parametri server
+const hostname = "127.0.0.1"
+const port = 3000
+
+// Oggetto risolutore del MIME type, data l'estensione file
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpeg': 'image/jpeg',
+    '.json': 'application/json',
+    '.js': 'text/javascript',
+    '.md': 'text/markdown'
+}
+
+// Oggetto risolutore delle path dei file, fornito l'URL della richiesta
+const filePaths = {
+    '/': './public/html/index.html',
+    '/css': './public/css/style.css',
+    '/logo_olimpiadi': './public/img/logo_olimpiadi.png'
+}
+
+function requestHandler(req, res) {
+    /* console.log(`>>> Richiesta in entrata. URL: ${req.url}`)
+    let reqPath = filePaths[req.url]
+    let extension = path.extname(reqPath)
+    let encoding = 'utf-8'
+
+    res.writeHead(200, { 'Content-Type': mimeTypes[extension] })
+    if (extension == '.png' || extension == '.jpeg') {
+        encoding = ''
+    } else {
+        encoding = 'utf-8'
+    }
+
+    fs.readFile(reqPath, encoding, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': mimeTypes['.html'] })
+            res.write(`<center><h1 style="border: 4px solid red;">Errore 404: risorsa non trovata.</h1></center>`)
+            res.end()
+        } else {
+            res.write(data)
+            res.end()
+        }
+    }) */
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, hostname, function () {
+    console.log(`Server in ascolto... http://${hostname}:${port}`)
+})
